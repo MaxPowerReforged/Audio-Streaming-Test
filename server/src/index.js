0c6e52c13132e958b1f01282c97d9a54dba8b981
MaxPowerEdain <56118011+MaxPowerEdain@users.noreply.github.com>
2021-02-07 12:15:54 +0100

    added support and config for static js in node server

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,22 @@
-const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const app = require('express')();
+const static = require('express-static');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on("connection", (socket) => {
-  socket.broadcast.emit("hi");
+app.use(static(__dirname + '/public'));
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 });
 
 http.listen(4000, () => {
-  console.log("listening on *:4000");
+  console.log('listening on *:4000');
 });
